701802dca6d7874144fd775e5564bf6d83439c9d
Nguyen Minh Nhat <nmnhat8668@gmail.com>
2022-11-02 09:17:00 +0700

    Update

--- lib/swaggers/generated/client_index.dart
+++ lib/swaggers/generated/client_index.dart
@@ -1,10 +1,1 @@
 export 'one_inch_aggregation.swagger.dart' show OneInchAggregation;
-export 'one_inch_aggregation_avalanche.swagger.dart'
-    show OneInchAggregationAvalanche;
-export 'one_inch_aggregation_binance.swagger.dart'
-    show OneInchAggregationBinance;
-export 'one_inch_aggregation_ethereum.swagger.dart'
-    show OneInchAggregationEthereum;
-export 'one_inch_aggregation_fantom.swagger.dart' show OneInchAggregationFantom;
-export 'one_inch_aggregation_polygon.swagger.dart'
-    show OneInchAggregationPolygon;
